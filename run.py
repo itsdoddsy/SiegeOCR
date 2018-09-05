583bba30458b747fce7de0db2544fe785bb40829
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-09-05 23:01:12 +0100

    Set timer image as an absolute path. Also multiply the SSIM value by 100 and only show one decimal.

--- run.py
+++ run.py
@@ -2,14 +2,19 @@ import cv2
 import numpy
 import mss
 from skimage.measure import compare_ssim as ssim
+import os
 
 
 def main():
     # Define our capture area
     area = {"left": 937, "top": 73, "width": 47, "height": 47}
 
+    # Get current directory of the script.
+    # Only needed because my IDE doesn't like relative directories.
+    # Works fine in a terminal without this, but it works with it too.
+    _CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
     # Open our timer picture to compare to
-    timer = cv2.imread('timer.png')
+    timer = cv2.imread(_CURRENT_DIR + '\\timer.png')
     # Convert it to a greyscale image
     timer = cv2.cvtColor(timer, cv2.COLOR_BGR2GRAY)
 
@@ -23,7 +28,7 @@ def main():
             # Display our capture. Not really necessary but
             # its nice to know what the program sees.
             cv2.imshow("Siege capture", capture)
-            print("SSIM: {:.2f}".format(ssim(capture, timer)))
+            print("SSIM: {:.1f}".format(ssim(capture, timer) * 100))
 
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 cv2.destroyAllWindows()
