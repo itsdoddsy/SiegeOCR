6bebac0161be438929d08792e62b8ab968c1eecf
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-09-05 22:30:33 +0100

    Open our timer comparison image and compare it to onscreen display. Won't work because one has an extra channel than the other. Fixed in next commit.

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy
 import mss
+from skimage.measure import compare_ssim as ssim
 
 
 def main():
     # Define our capture area
     area = {"left": 937, "top": 73, "width": 47, "height": 47}
+
+    # Open our timer picture to compare to
+    timer = cv2.imread('timer.png')
 
     # Now grab that area
     with mss.mss() as sct:
@@ -15,6 +19,7 @@ def main():
             # Display our capture. Not really necessary but
             # its nice to know what the program sees.
             cv2.imshow("Siege capture", capture)
+            print("SSIM: {:.2f}".format(ssim(capture, timer)))
 
             if cv2.waitKey(25) & 0xFF == ord("q"):
                 cv2.destroyAllWindows()
